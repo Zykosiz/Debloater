a354e7bc6b9f92f6b14500ff0471dd9c231ef58b
Zykosiz <zulanu@protonmail.com>
2026-01-14 15:07:29 +0100

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -4,7 +4,8 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco feature enable -n=allowGlobalConfirmation
 choco install googlechrome --ignore-checksums
 choco install adobereader
-Invoke-WebRequest "https://customdesignservice.teamviewer.com/download/windows/v15/6kyy3pe/TeamViewerQS.exe?sv=2023-11-03&se=2026-01-15T13%3A44%3A17Z&sr=b&sp=r&sig=aWS7FguKRFZ%2FFyHjF8tYU66yvZDxFPQdlwMk8IJ%2FLYc%3D&1768398256874" -OutFile "$env:userprofile\desktop\netip.exe"
+Invoke-WebRequest "https://get.teamviewer.com/6nsd5xz" -OutFile "C:\Temp\Netip.exe"
+Move-Item -Path "C:\Temp\Netip.exe" -Destination ""$env:userprofile\desktop\netip.exe"" -Force
 
 
 
