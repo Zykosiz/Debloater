6229471c0050e1b665a9d2f37153899bc6669bc4
Zykosiz <zulanu@protonmail.com>
2026-01-14 14:31:44 +0100

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -1,4 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
-choco install chrome
+choco install googlechrome
+choco install adobereader
+Invoke-WebRequest "https://get.teamviewer.com/6kyy3pe" -OutFile "c:\users\%username%\desktop\Netip Support.exe"
